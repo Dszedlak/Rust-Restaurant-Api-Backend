3c7570bbd0ddd4f55283d0ba18eccacde7d39214
David Szedlak <Dszedlak@gmail.com>
2022-05-25 01:07:48 +0100

    Unit tests added

--- target/debug/deps/rust_restaurant_api-8544578c9d0e2966.d
+++ target/debug/deps/rust_restaurant_api-8544578c9d0e2966.d
@@ -1,11 +1,11 @@
-c:\Users\DAVID\Documents\GitHub\rust-example\target\debug\deps\rust_restaurant_api-8544578c9d0e2966.rmeta: src\main.rs src\handlers.rs src\db.rs src\models.rs src\error_handler.rs \\?\C:\Users\DAVID\Documents\GitHub\rust-example\migrations\20220515082502_create_tables.sql \\?\C:\Users\DAVID\Documents\GitHub\rust-example\migrations\20220516013836_insert_data.sql
+c:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\target\debug\deps\rust_restaurant_api-8544578c9d0e2966.rmeta: src\main.rs src\handlers.rs src\db.rs src\models.rs src\error_handler.rs \\?\C:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\migrations\20220515082502_create_tables.sql \\?\C:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\migrations\20220516013836_insert_data.sql
 
-c:\Users\DAVID\Documents\GitHub\rust-example\target\debug\deps\rust_restaurant_api-8544578c9d0e2966.d: src\main.rs src\handlers.rs src\db.rs src\models.rs src\error_handler.rs \\?\C:\Users\DAVID\Documents\GitHub\rust-example\migrations\20220515082502_create_tables.sql \\?\C:\Users\DAVID\Documents\GitHub\rust-example\migrations\20220516013836_insert_data.sql
+c:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\target\debug\deps\rust_restaurant_api-8544578c9d0e2966.d: src\main.rs src\handlers.rs src\db.rs src\models.rs src\error_handler.rs \\?\C:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\migrations\20220515082502_create_tables.sql \\?\C:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\migrations\20220516013836_insert_data.sql
 
 src\main.rs:
 src\handlers.rs:
 src\db.rs:
 src\models.rs:
 src\error_handler.rs:
-\\?\C:\Users\DAVID\Documents\GitHub\rust-example\migrations\20220515082502_create_tables.sql:
-\\?\C:\Users\DAVID\Documents\GitHub\rust-example\migrations\20220516013836_insert_data.sql:
+\\?\C:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\migrations\20220515082502_create_tables.sql:
+\\?\C:\Users\DAVID\Documents\GitHub\Rust-Restaurant-Api-Backend\migrations\20220516013836_insert_data.sql:
